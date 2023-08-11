496d00564ecaf47858a4dd20acbdc994ebe93c39
maycon.freitas@norhart.com <maycon.freitas@norhart.com>
2023-08-11 13:30:54 -0300

    JB's logic

--- phone-check.js
+++ phone-check.js
@@ -1,7 +1,32 @@
 const phoneNumberColumns = ['TELEFONE - CELULAR', 'TELEFONE - COMERCIAL', 'TELEFONE - RESIDENCIAL', 'TELEFONE - EXTRA'];
 
+function calculateAge(birthday) {
+    let today = new Date();
+    let birthDate = new Date(birthday);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let month = today.getMonth() - birthDate.getMonth();
+
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+function isMemberJb(age) {
+    if (age < 18) {
+        return true;
+    }
+    return false;
+}
+
+
 function checkPhoneNumber(df, inputPhoneNumber) {
     for (let i = 0; i < df.shape[0]; i++) {
+
+        let birthday = df.at(i, 'DATA DE NASCIMENTO');
+        let age = calculateAge(birthday);
+
         for (let column of phoneNumberColumns) {
             let phoneNumber = df.at(i, column);
 
@@ -25,18 +50,18 @@ function checkPhoneNumber(df, inputPhoneNumber) {
                     // If it has 13 digits, it includes the ninth digit. Let's try without it too
                     let numberWithoutNinthDigit = phoneNumber.slice(0, 4) + phoneNumber.slice(5);
                     if (phoneNumber === inputPhoneNumber || numberWithoutNinthDigit === inputPhoneNumber) {
-                        return {found: true, status: status};
+                        return {found: true, status: status, isMemberJb: isMemberJb(age)};
                     }
                 } else if (phoneNumber.length === 12) {
                     // If it has 12 digits, it doesn't include the ninth digit. Let's try with it too
                     let numberWithNinthDigit = phoneNumber.slice(0, 4) + '9' + phoneNumber.slice(4);
                     if (phoneNumber === inputPhoneNumber || numberWithNinthDigit === inputPhoneNumber) {
-                        return {found: true, status: status};
+                        return {found: true, status: status, isMemberJb: isMemberJb(age)};
                     }
                 }
             } else {
                 if (phoneNumber === inputPhoneNumber) {
-                    return {found: true, status: status};
+                    return {found: true, status: status, isMemberJb: isMemberJb(age)};
                 }
             }
         }
