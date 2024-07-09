ebc1a024909a5fcd8390b7b60372b64b98cc8bd7
Thiago Santos <contato@tsantos.com.br>
2024-07-09 20:37:04 +0100

    wip

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ const client = new Client({
         "--no-sandbox",
         "--disable-gpu",
       ],
-    },
-    webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', }
+    }
 });
 
 client.on('qr', qr => {
