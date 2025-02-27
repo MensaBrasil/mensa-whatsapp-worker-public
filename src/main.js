9f88b523264086f0cae84e6e0f6038472ce066b3
Hiago Teixeira <hiagodevsolutions@gmail.com>
2025-02-27 18:08:00 -0300

    Remove unused to fix lint

--- src/main.js
+++ src/main.js
@@ -4,15 +4,6 @@ import qrcode from 'qrcode-terminal';
 import WAWebJS from 'whatsapp-web.js';
 const { Client, LocalAuth } = WAWebJS;
 
-import { addMembersToGroups } from './core/addQueue.mjs';
-import { fetchMessagesFromGroups } from './core/fetchMessagesMode.mjs';
-import { removeMembersFromGroups } from './core/removeQueue.mjs';
-import { reportMembersInfo } from './core/reportMode.mjs';
-import { scanGroups } from './core/scanMode.mjs';
-import { getPhoneNumbersWithStatus, saveGroupsToList } from './database/pgsql.mjs';
-import { processGroups } from './utils/groups.mjs';
-import { preprocessPhoneNumbers, checkPhoneNumber } from './utils/phone-check.mjs';
-
 configDotenv();
 
 // Global error handler
@@ -20,29 +11,6 @@ process.on('unhandledRejection', (reason) => {
   console.error('Unhandled Rejection:', reason);
   process.exit(1);
 });
-
-// Cli args
-const modes = {
-  '--add': 'Add',
-  '--remove': 'Remove',
-  '--scan': 'Scan',
-  '--report': 'Report',
-  '--fetch': 'Fetch Messages',
-};
-const selected = Object.entries(modes).reduce((acc, [flag, name]) => (process.argv.includes(flag) ? acc.concat(name) : acc), []);
-
-const addMode = process.argv.includes('--add');
-const removeMode = process.argv.includes('--remove');
-const scanMode = process.argv.includes('--scan');
-const reportMode = process.argv.includes('--report');
-const fetchMessagesMode = process.argv.includes('--fetch');
-
-if (!selected.length) {
-  console.error('You should select at least 1 service. Exiting...\nPlease choose from ' + Object.keys(modes).join(', '));
-  process.exit(1);
-}
-
-console.log('Services selected:', selected.join(', '));
 
 // Initialize client
 const client = new Client({
@@ -73,55 +41,6 @@ client.on('ready', async () => {
   client.setAutoDownloadVideos(false);
 
   console.log('Client is ready!');
-
-  // Initial Setup
-  const { chats, groups } = await processGroups(client);
-  const groupNames = groups.map((group) => group.name);
-  const groupIds = groups.map((group) => group.id._serialized);
-
-  if (chats.length === 0 || groups.length === 0) {
-    console.error('No groups found. Exiting.');
-    process.exit(1);
-  }
-
-  await saveGroupsToList(groupNames, groupIds);
-
-  // Initial checks
-  const phoneNumbersFromDB = preprocessPhoneNumbers(await getPhoneNumbersWithStatus());
-  if (phoneNumbersFromDB.length === 0) {
-    console.error('No phone numbers found in the database. Exiting.');
-    process.exit(1);
-  }
-
-  const checkResult = checkPhoneNumber(phoneNumbersFromDB, process.env.SANITY_CHECK);
-  if (!checkResult.found) {
-    console.error(`Number ${process.env.SANITY_CHECK} not found in the database. Sanity check failed. Exiting.`);
-    process.exit(1);
-  }
-
-  if (scanMode) {
-    console.log('Scanning groups...');
-    await scanGroups(groups, phoneNumbersFromDB);
-  }
-
-  if (reportMode) {
-    await reportMembersInfo(client, chats, groups, phoneNumbersFromDB);
-  }
-
-  if (fetchMessagesMode) {
-    console.log('Fetching messages...');
-    await fetchMessagesFromGroups(groups, phoneNumbersFromDB);
-  }
-
-  if (addMode) {
-    console.log('Adding members...');
-    await addMembersToGroups(groups);
-  }
-
-  if (removeMode) {
-    console.log('Removing members...');
-    await removeMembersFromGroups(groups, phoneNumbersFromDB);
-  }
 
   console.log('All tasks completed. Exiting...');
   await fetch(process.env.UPTIME_URL);
