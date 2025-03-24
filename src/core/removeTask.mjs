268310aa2d9c5ce563268b9586387d7bacff6e5d
Hiago Teixeira <hiagodevsolutions@gmail.com>
2025-03-24 15:44:06 -0300

    Moved delay logic from main to each task

--- src/core/removeTask.mjs
+++ src/core/removeTask.mjs
@@ -1,8 +1,15 @@
+import { configDotenv } from 'dotenv';
 import WAWebJS from 'whatsapp-web.js'; // eslint-disable-line no-unused-vars
 
 import { recordUserExitFromGroup } from '../database/pgsql.mjs';
 import { getFromRemoveQueue } from '../database/redis.mjs';
 import { removeMemberFromGroup } from '../utils/clientOperations.mjs';
+import { delay } from '../utils/misc.mjs';
+
+configDotenv();
+
+const removeDelay = Number(process.env.REMOVE_DELAY) || 10;
+const delayOffset = Number(process.env.DELAY_OFFSET) || 3;
 
 /**
  * Processes one item from the removeQueue and returns success or failure
@@ -25,11 +32,13 @@ async function processRemoveQueue(client) {
     if (result.removed && result.removalType === 'Community') {
         console.log(`\x1b[92mMember ${item.phone} successfully removed from community ${item.groupId} -> ${result.groupName} for reason: ${item.reason}\x1b[0m`);
         await recordUserExitFromGroup(item.phone, item.communityId, item.reason);
+        await delay(removeDelay, delayOffset);
         return true;
     }
     else if (result.removed && result.removalType === 'Group') {
         console.log(`\x1b[92mMember ${item.phone} successfully removed from group ${item.groupId} -> ${result.groupName} for reason: ${item.reason}\x1b[0m`);
         await recordUserExitFromGroup(item.phone, item.groupId, item.reason);
+        await delay(removeDelay, delayOffset);
         return true;
     }
     return false;
